8bd2369317e61f065dc575c4a1edb74334a7eaa6
Hansjason97 <hansadiogo@gmail.com>
2023-06-26 10:27:09 +0100

    working on the navigation

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
-import logo from './assets/logo.svg'
+import React, {useState} from 'react'
 
 function Header() {
+
+  const [visible, setVisible] = useState(false);
+
+  let menuClass = 'h-full w-fit bg-secondaryDark fixed top-[80px] right-0 z-10 flex flex-col gap-12 py-8 font-mono text-center px-[70px] md:hidden transition-all duration-300 ease-in-out counterLink';
+  if (!visible) menuClass='h-full w-fit bg-secondaryDark fixed top-[80px] right-[-100%] flex flex-col gap-12 py-8 font-mono text-center px-[70px] md:hidden z-10 transition-all duration-300 ease-in-out';
+
   return (
-    <div className='px-5 h-[100px] flex items-center justify-between lg:px-[50px] fixed top-0 left-0 w-full'>
+    <div className='px-5 h-[80px] flex items-center justify-between lg:px-[50px] fixed top-0 left-0 w-full bg-secondary z-20 shadow-lg'>
         <nav>
-           <img src={logo} alt="logo" className='h-[40px]' /> 
+        <a href="" className='w-fit px-3 text-xl text-primary font-semi-bold'>HA.</a>
         </nav>
 
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 text-primary lg:hidden">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
-        </svg>
+        <div className={ visible ? `menuIcon menuIconActive` : `menuIcon`} onClick={()=>setVisible(!visible)}>
+          <div></div>
+        </div>
+        {/**Mobile Menu */}
+     <nav className={menuClass}>
+        <a href='#about' className='menu-mobile-link'>About</a>
+        <a href='#experience' className='menu-mobile-link'>Experience</a>
+        <a href='#work' className='menu-mobile-link'>Projects</a>
+        <a href='#contact' className='menu-mobile-link'>Contact</a>
+     </nav>
     </div>
   )
 }
