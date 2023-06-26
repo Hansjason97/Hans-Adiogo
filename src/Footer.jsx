d8b80e831b035b372230f3f14d24c9617397ed25
Hansjason97 <hansadiogo@gmail.com>
2023-06-26 03:47:45 +0100

    fixed footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -7,18 +7,20 @@ import { FaBehance } from 'react-icons/fa'
 function Footer() {
   return (
     <div className=''>
-        <div className='flex gap-8 text-xl w-fit mx-auto px-5 pb-10 z-20 items-center
+        <div className='flex gap-8 text-xl w-fit mx-auto px-5 pb-10 z-20 items-center transition-all duration-100 ease-out
         md:fixed md:left-0 md:bottom-0 md:flex-col md:px-2 md:gap-4 md:py-0
-        md:after:content-[""] md:after:block md:after:h-10 md:after:w-[1px] md:after:bg-blancDark'>
+        md:after:content-[""] md:after:block md:after:h-10 md:after:w-[1px] md:after:bg-blancDark
+        lg:left-6'>
             <a href="https://github.com/Hansjason97" className='footerLink'><VscGithub /></a>
             <a href="https://www.linkedin.com/in/jason-adiogo-a12018199" className='footerLink'><RxLinkedinLogo /></a>
             <a href="https://www.instagram.com/jasonadiogo/" className='footerLink'><GrInstagram /></a>
             <a href="https://www.behance.net/hanspen" className='footerLink'><FaBehance /></a>
         </div>
-        <div className='hidden z-20 px-5 pb-10 md:flex md:items-center
+        <div className='hidden z-20 px-5 pb-10 md:flex md:items-center transition-all duration-100 ease-out
         md:fixed md:right-1 md:bottom-0 md:flex-col md:px-2 md:gap-4 md:py-0
-        md:after:content-[""] md:after:block md:after:h-10 md:after:w-[1px] md:after:bg-blancDark'>
-            <a href="mailto:hansadiogo@gmail.com" className='font-mono tracking-wider text-sm' style={{writingMode: 'vertical-rl'}}>hansadiogo@gmail.com</a>
+        md:after:content-[""] md:after:block md:after:h-10 md:after:w-[1px] md:after:bg-blancDark
+        lg:right-7'>
+            <a href="mailto:hansadiogo@gmail.com" className='footerMail'>hansadiogo@gmail.com</a>
         </div>
     </div>
   )
