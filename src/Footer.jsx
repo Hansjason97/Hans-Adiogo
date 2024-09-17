2c010cabcad4799bf364752ef99d9e3024b11121
Nguezet Jordan Junior <kenfackjordanjunior@gmail.com>
2024-09-17 17:04:28 +0100

    contact mail updated to contact@krestdev.com

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,32 +1,51 @@
-import React from 'react'
-import { VscGithub } from 'react-icons/vsc'
-import { RxLinkedinLogo } from 'react-icons/rx'
-import { GrInstagram } from 'react-icons/gr'
-import { FaBehance } from 'react-icons/fa'
+import React from "react";
+import { VscGithub } from "react-icons/vsc";
+import { RxLinkedinLogo } from "react-icons/rx";
+import { GrInstagram } from "react-icons/gr";
+import { FaBehance } from "react-icons/fa";
 
 function Footer() {
   return (
-    <div className=''>
-        <div className='flex gap-8 text-xl w-fit mx-auto px-5 z-20 items-center transition-all duration-100 ease-out
+    <div className="">
+      <div
+        className='flex gap-8 text-xl w-fit mx-auto px-5 z-20 items-center transition-all duration-100 ease-out
         md:fixed md:left-0 md:bottom-0 md:flex-col md:px-2 md:gap-4 md:py-0
         md:after:content-[""] md:after:block md:after:h-10 md:after:w-[1px] md:after:bg-blancDark
-        lg:left-6'>
-            <a href="#" className='footerLink'><VscGithub /></a>
-            <a href="#" className='footerLink'><RxLinkedinLogo /></a>
-            <a href="#" className='footerLink'><GrInstagram /></a>
-            <a href="#" className='footerLink'><FaBehance /></a>
-        </div>
-        <div className='hidden z-20 px-5 pb-10 md:flex md:items-center transition-all duration-100 ease-out
+        lg:left-6'
+      >
+        <a href="#" className="footerLink">
+          <VscGithub />
+        </a>
+        <a href="#" className="footerLink">
+          <RxLinkedinLogo />
+        </a>
+        <a href="#" className="footerLink">
+          <GrInstagram />
+        </a>
+        <a href="#" className="footerLink">
+          <FaBehance />
+        </a>
+      </div>
+      <div
+        className='hidden z-20 px-5 pb-10 md:flex md:items-center transition-all duration-100 ease-out
         md:fixed md:right-1 md:bottom-0 md:flex-col md:px-2 md:gap-4 md:py-0
         md:after:content-[""] md:after:block md:after:h-10 md:after:w-[1px] md:after:bg-blancDark
-        lg:right-7'>
-            <a href="mailto:gviche@krestholding.com" className='footerMail'>gviche@krestholding.com</a>
-        </div>
-        <div className='flex py-5 justify-center gap-5 font-mono text-sm'>
-          <a href="https://github.com/Hansjason97" className='transition-all duration-100 ease-linear hover:text-primary'>2023 - Krest Digital & FinTech</a>
-        </div>
+        lg:right-7'
+      >
+        <a href="mailto:contact@krestdev.com" className="footerMail">
+          contact@krestdev.com
+        </a>
+      </div>
+      <div className="flex py-5 justify-center gap-5 font-mono text-sm">
+        <a
+          href="https://github.com/Hansjason97"
+          className="transition-all duration-100 ease-linear hover:text-primary"
+        >
+          2023 - Krest Digital & FinTech
+        </a>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,67 +1,62 @@
-import React from 'react'
-import FeaturedProjects from '../components/FeaturedProjects'
-import Reveal from '../components/Reveal'
-import { featuredProjects } from '../data/data'
+import React from "react";
+import FeaturedProjects from "../components/FeaturedProjects";
+import Reveal from "../components/Reveal";
+import { featuredProjects } from "../data/data";
 
-import { useTranslation } from "react-i18next"
+import { useTranslation } from "react-i18next";
 
 function home() {
-
   const { t } = useTranslation();
 
   return (
     <div>
       {/* First section ############################################################## */}
       <Reveal>
-      <section id="home" className='min-h-[100vh] px-5 md:px-[50px] lg:px-[100px] mx-auto max-w-5xl flex flex-col justify-center'>
-        <p className='special mb-1'>{t('hi')}</p>
-        <h2 className='lvlOne text-blanc'>
-          KrestDev.
-        </h2>
-        <h2 className='lvlOne'>
-          {t('titleHome')}
-        </h2>
+        <section
+          id="home"
+          className="min-h-[100vh] px-5 md:px-[50px] lg:px-[100px] mx-auto max-w-5xl flex flex-col justify-center"
+        >
+          <p className="special mb-1">{t("hi")}</p>
+          <h2 className="lvlOne text-blanc">KrestDev.</h2>
+          <h2 className="lvlOne">{t("titleHome")}</h2>
 
-        <p className='mt-5 max-w-lg'>
-          {t('desc')}
-        </p>
-          <button className='mt-6 button-primary'>
-            <a href="mailto:gviche@krestholding.com">{t('contactMe')}</a>
+          <p className="mt-5 max-w-lg">{t("desc")}</p>
+          <button className="mt-6 button-primary">
+            <a href="mailto:contact@krestdev.com">{t("contactMe")}</a>
           </button>
-      </section>
+        </section>
       </Reveal>
       {/**Second section #############################################################"" */}
       <Reveal>
-      <section id='about' className='containerBase'>
-        <h2 className='numbered-heading'>
-          {t('aboutMe')}
-        </h2>
-        <div className='flex flex-col md:flex-row gap-[50px]'>
-          <div className='lg:w-[512px] flex flex-col gap-4'>
-            <p>
-              {t('aboutTxt1')}{/**<a href="" className='customLink'>figma</a>/
-               */}
-            </p>
-            <p>
-            {t('aboutTxt2')}
-            </p>
-             <p>
-            {t('aboutTxt3')}
-            </p>
-            <ul role='list' className='technologies'>
+        <section id="about" className="containerBase">
+          <h2 className="numbered-heading">{t("aboutMe")}</h2>
+          <div className="flex flex-col md:flex-row gap-[50px]">
+            <div className="lg:w-[512px] flex flex-col gap-4">
+              <p>
+                {t("aboutTxt1")}
+                {/**<a href="" className='customLink'>figma</a>/
+                 */}
+              </p>
+              <p>{t("aboutTxt2")}</p>
+              <p>{t("aboutTxt3")}</p>
+              <ul role="list" className="technologies">
                 <li>Javascript</li>
                 <li>React</li>
                 <li>Figma</li>
                 <li>Laravel</li>
                 <li>Angular</li>
               </ul>
+            </div>
+            <div className="mx-auto sm:mx-0 relative top-0 max-h-[300px] w-fit cursor-pointer group transition-all duration-300 ease-in hover:top-[-7px]">
+              <div className="absolute h-full w-full z-[2] transition-all duration-150 ease-linear mix-blend-color hover:opacity-0"></div>
+              <img
+                src="/images/krestdev.png"
+                alt=""
+                className="max-w-[300px] object-cover contrast-50 group-hover:contrast-100 grayscale group-hover:grayscale-0 transition-all duration-150 ease-linear"
+              />
+            </div>
           </div>
-          <div className='mx-auto sm:mx-0 relative top-0 max-h-[300px] w-fit cursor-pointer group transition-all duration-300 ease-in hover:top-[-7px]'>
-            <div className='absolute h-full w-full z-[2] transition-all duration-150 ease-linear mix-blend-color hover:opacity-0'></div>
-            <img src="/images/krestdev.png" alt="" className='max-w-[300px] object-cover contrast-50 group-hover:contrast-100 grayscale group-hover:grayscale-0 transition-all duration-150 ease-linear' />
-          </div>
-        </div>
-      </section>
+        </section>
       </Reveal>
       {/**Third section ################################################################## */}
       {/* <Reveal>
@@ -74,33 +69,29 @@ function home() {
       </Reveal> */}
       {/**Fourth Section ################################################################# */}
       <Reveal>
-      <section id='work' className='containerBase'>
-        <h2 className='numbered-heading lg:w-fit lg:max-w-5xl lg:mx-auto'>
-          {t('projectsSection')}
-        </h2>
-        <p className='max-w-sm text-xs'>{t('Added')}</p>
-        <FeaturedProjects data={featuredProjects} />
-      </section>
+        <section id="work" className="containerBase">
+          <h2 className="numbered-heading lg:w-fit lg:max-w-5xl lg:mx-auto">
+            {t("projectsSection")}
+          </h2>
+          <p className="max-w-sm text-xs">{t("Added")}</p>
+          <FeaturedProjects data={featuredProjects} />
+        </section>
       </Reveal>
       {/**Contact Section ################################################################# */}
       <Reveal>
-      <section id='contact' className='containerBase'>
-        <p className='special text-center mb-1'>
-          {t('contactTop')}
-        </p>
-        <h2 className='text-3xl sm:text-5xl leading-[110%] tracking-tight mb-8 text-blanc text-center'>
-          {t('contactTitle')}
-        </h2>
-        <p className='mx-auto max-w-lg text-center'>
-          {t('contactContent')}
-        </p>
-        <button className='my-10 button-primary mx-auto'>
-            <a href="mailto:gviche@krestholding.com">{t('messageButton')}</a>
+        <section id="contact" className="containerBase">
+          <p className="special text-center mb-1">{t("contactTop")}</p>
+          <h2 className="text-3xl sm:text-5xl leading-[110%] tracking-tight mb-8 text-blanc text-center">
+            {t("contactTitle")}
+          </h2>
+          <p className="mx-auto max-w-lg text-center">{t("contactContent")}</p>
+          <button className="my-10 button-primary mx-auto">
+            <a href="mailto:contact@krestdev.com">{t("messageButton")}</a>
           </button>
-      </section>
+        </section>
       </Reveal>
     </div>
-  )
+  );
 }
 
-export default home
+export default home;
